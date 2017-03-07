70b57117747ed1bc735ecfb27093b91a61707c8d
Alex Lopatin <lopatin.rnd@gmail.com>
2017-03-07 09:52:07 +0300

    added right_triangle

---
+++ lesson_1/right_triangle.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+puts "Please, enter first side of triangle."
+a = gets.chomp.to_i
+puts "Please, enter second side of triangle."
+b = gets.chomp.to_i
+puts " Please, enter third side of triangle."
+c = gets.chomp.to_i
+if a >= b && a >= c
+  hipotenuse = a
+  cathetus_one = b
+  cathetus_two = c
+elsif b >= a && b >= c
+  hipotenuse = b
+  cathetus_one = a
+  cathetus_two = c
+elsif c >= a && c >= b
+  hipotenuse = c
+  cathetus_one = a
+  cathetus_two = b
+end
+if a == b || a == c || c == b
+  puts "Triangle is isosceles triangle (равнобедренный)"
+end
+if a == b && a == c
+  puts "Triangle is equilateral.(равносторонний)"
+end
+if hipotenuse ** 2 == (cathetus_one ** 2) + (cathetus_two ** 2)
+  puts "Triangle is right (прямоугольный)"
+end
+
