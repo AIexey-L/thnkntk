10702a7dced8359cfddb6decc85eed3f1632df78
Alex Lopatin <lopatin.rnd@gmail.com>
2017-03-09 22:56:12 +0300

    completed 6th task

--- lesson_2/sumofsales.rb
+++ lesson_2/sumofsales.rb
@@ -2,17 +2,20 @@
 basket = Hash.new { |h, product| h[product] = Hash.new { |hh, price| hh[price] = qty } }
 # by_product = { product => [price, qty] }
 by_product_sum = Hash.new { |product, k| product[k] = Array.new }
+total = 0
 loop do
   puts 'Enter product name'
   puts 'or enter stop for result'
   product = gets.chomp.to_s
   break if product == 'stop'
-puts 'Enter product price'
-price = gets.chomp.to_f
-puts 'Enter product quantity'
-qty = gets.chomp.to_f
-basket[product][price] = qty
-by_product_sum[product] = basket[product].to_a.reduce(:*)
+  puts 'Enter product price'
+  price = gets.chomp.to_f
+  puts 'Enter product quantity'
+  qty = gets.chomp.to_f
+  basket[product][price] = qty
+  by_product_sum[product] = basket[product].to_a.map { |x| x.reduce(:*)}
+  total = by_product_sum.values.flatten.reduce(:+)
 end
 p basket
-p by_product
+p by_product_sum
+p total
