741caf202137a3c7bf55bec6d18f132f1993e9b9
Alex Lopatin <lopatin.rnd@gmail.com>
2017-03-23 13:44:44 +0300

    add Acessors module

---
+++ lesson_9/acessors.rb
@@ -0,0 +1,26 @@
+module Acessors
+
+  @@history = {}
+  
+  def attr_accessor_with_history(*args)
+    args.each do |name|
+      var_name = "@#{name}".to_sym
+      @@history[var_name] = []
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) do |value|
+        instance_variable_set(var_name, value)
+        @@history[var_name] << value
+      end
+      define_method("#{name}_history") { @@history[var_name] }
+    end
+  end
+
+  def strong_attr_acessor(name, type)
+    var_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) do |value|
+      raise 'wrong variable type' unless value.class.to_s == type.to_ses
+      instance_variable_set(var_name, value)
+    end
+  end
+end
